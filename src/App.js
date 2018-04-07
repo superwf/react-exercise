c64a892c10147c02ae3720dc09ff7761c544c9de
superwf <superwf@gmail.com>
2018-04-07 18:28:07 +0800

    test: add jest resolve with webpack alias
    move webpack common config to config/webpack.config.base.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import React, { Component } from 'react'
-import { Tree } from 'antd'
-import logo from './logo.svg'
-import './App.css'
-import 'antd/dist/antd.css'
-
-const TreeNode = Tree.TreeNode
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   render() {
@@ -17,43 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Tree
-          checkable
-          defaultCheckedKeys={['0-0-0', '0-0-1-0']}
-          defaultExpandAll
-          onSelect={this.onSelect}
-          onCheck={this.onCheck}
-        >
-          <TreeNode title="parent 1" key="0-0">
-            <TreeNode title="parent 1-0" key="0-0-0" disabled>
-              <TreeNode title="leaf" key="0-0-0-1" />
-            </TreeNode>
-            <TreeNode title="parent 1-1" key="0-0-1">
-              <TreeNode
-                title={<span style={{ color: '#1890ff' }}>sss</span>}
-                key="0-0-1-0"
-              />
-              <TreeNode
-                title={<span style={{ color: '#1890ff' }}>sss</span>}
-                key="0-0-1-1"
-              />
-              <TreeNode
-                title={<span style={{ color: '#1890ff' }}>sss</span>}
-                key="0-0-1-2"
-              />
-            </TreeNode>
-          </TreeNode>
-        </Tree>
       </div>
-    )
-  }
-
-  onSelect = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info)
-  }
-  onCheck = (checkedKeys, info) => {
-    console.log('onCheck', checkedKeys, info)
+    );
   }
 }
 
-export default App
+export default App;
